cf72bfe11f700ce238692be2560d05167c647478
Andres <andimier@gmail.com>
2020-12-13 17:46:04 -0500

    BE - adjust login functionality

--- cms/noticias.php
+++ cms/noticias.php
@@ -1,55 +1,56 @@
-<?php 
-require_once("includes/sesion.php");
-require_once("includes/connection.php");
-require_once("includes/functions.php");
-encontrar_seccion_y_contenido_seleccionados();
-require_once("editor.php");
+<?php
+	require_once("includes/sesion.php");
+	require_once("includes/connection.php");
+	require_once("../utils/phpfunctions.php");
+	require_once("includes/functions.php");
+	encontrar_seccion_y_contenido_seleccionados();
+	require_once("editor.php");
 
 $mensaje = "";
 
 if(isset($_POST['insertarnoticia'])){
 	$titulo = $_POST['titulo'];
-	
+
 	$errores = array();
 	$required_fields = array('titulo');
 	$imagenprovisional = "imagenes/pequenas/camara_01.png";
-	
+
 	foreach($required_fields as $fieldname){
 		if(!isset($_POST[$fieldname]) || (empty($_POST[$fieldname])  && !is_numeric($_POST[$fieldname]))){
-			$errores[] = $fieldname;	
+			$errores[] = $fieldname;
 		}
 	}
-	
+
 	if(empty($errores)){
 		$query = "INSERT INTO noticias (titulo, imagen1, imagen2, imagen3) VALUES ('$titulo', '$imagenprovisional', '$imagenprovisional', '$imagenprovisional')";
 		if($result = mysql_query($query, $connection)){
-			$mensaje = "La publicacion se ha creado correctamente";	
+			$mensaje = "La publicacion se ha creado correctamente";
 		}
 	}else{
 			$mensaje = "Debes ingresar un titulo!!";
 		}
-	
+
 }
-	
-require_once("cabeza.php");?>    
+
+require_once("cabeza.php");?>
 
 <div class="col2">
 	<h3>Noticias</h3>
-	
+
 	Has click sobre el titulo del contenido para editarlo <br />
-	
+
     <div class="mensaje" style="color:#F00"> <?php echo $mensaje; ?></div>
     <br />
-   
+
 	<?php
 	$grupo_noticias = todas_las_noticias();
-	
-	
+
+
     while($not = mysql_fetch_array($grupo_noticias)){
 		echo "<a href=\"editar-noticia.php?noticia_id=" . urlencode($not["id"]) . "\">{$not["fecha"]} &nbsp;&nbsp; {$not["titulo"]}</a><br /><br />";
 	}
 	?>
-   
+
     <hr />
     <h3>Insertar nueva Publicacion</h3>
     Titluo:
